dc516bd7258e193a616877438ead73fe0d47decf
Shining-Thunder520 <yuuna1560@gmail.com>
2026-06-18 00:43:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+function openPage(pageId){
+    document.getElementById("homePage").classList.add("hidden");
+    document.getElementById(pageId).classList.remove("hidden");
+}
 
+function goHome(){
+    document.getElementById("homePage").classList.remove("hidden");
+
+    document.querySelectorAll(".subPage").forEach(page=>{
+        page.classList.add("hidden");
+    });
+}
